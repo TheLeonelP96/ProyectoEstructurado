3c04c17e897405d971d2b31bc48caa3e5930dcb0
TheLeonelP96 <p.leonel527@gmail.com>
2025-04-03 11:26:46 -0400

    Proyecto Estructurado

---
+++ Vistas/crearEstudiante.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <h2>Registro de Estudiante</h2>
+        <div class="row">
+            <div class="col-md-6">
+                <form action="form">
+                    <div class="form-group">
+                        <label for="">Nombre</label>
+                        <input class="form-control" type="text">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Apellidp</label>
+                        <input class="form-control" type="text">
+                    </div>
+                    <div class="form-group">
+                        <label for="">CI</label>
+                        <input class="form-control" type="text">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Genero</label>
+                        <select class="form-selec" name="" id="">
+                            <option value="">Masculino</option>
+                            <option value="">Femenino</option>
+                            <option value="">Helicoptero</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Curso</label>
+                        <input class="form-control" type="text">
+                    </div>
+                    <div class="formulario-group">
+                        <input class="btn btn-primary" type="button" value="Guardar">
+                    </div>
+                    
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
